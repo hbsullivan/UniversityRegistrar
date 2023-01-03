31c7b07fe35328328b70fdd4ab353952b0879f54
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-03 13:59:45 -0800

    can track students enrolled in university
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

---
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+
+    public StudentsController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Students.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+  }
+}
