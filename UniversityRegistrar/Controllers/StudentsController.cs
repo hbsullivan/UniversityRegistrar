ea2394096e2ca183726aeb07241277dc7a6d4a98
Henry Sullivan <sullivanbhenry@gmail.com>
2023-01-04 10:08:48 -0800

    can view both sides of many to many relationship
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using UniversityRegistrar.Models;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace UniversityRegistrar.Controllers
 {
@@ -36,8 +37,31 @@ namespace UniversityRegistrar.Controllers
     public ActionResult Details(int id)
     {
       Student thisStudent = _db.Students
+                            .Include(student => student.JoinEntities)
+                            .ThenInclude(join => join.Course)
                             .FirstOrDefault(student => student.StudentId == id);
       return View(thisStudent);
+    }
+
+    public ActionResult AddCourse(int id)
+    {
+      Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+      return View(thisStudent);
+    }
+
+    [HttpPost]
+    public ActionResult AddCourse(Student student, int courseId)
+    {
+      #nullable enable
+      CourseStudent? joinEntity = _db.CourseStudents.FirstOrDefault(joinEntity => (joinEntity.CourseId == courseId && joinEntity.StudentId == student.StudentId));
+      #nullable disable
+      if (joinEntity == null && courseId != 0)
+      {
+        _db.CourseStudents.Add(new CourseStudent() { CourseId = courseId, StudentId = student.StudentId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = student.StudentId });
     }
 
   }
